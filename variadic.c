f581dfdabc85f4c9a5d03b6b716392cc9cb54def
linzhenghao233 <1392872316@qq.com>
2023-08-23 23:12:05 +0800

    添加项目文件。

---
+++ variadic.c
@@ -0,0 +1,15 @@
+/* variadic.c -- ±ä²Îºê */
+#include <stdio.h>
+#include <math.h>
+#define PR(x, ...) printf("Message " #x ": " __VA_ARGS__)
+
+int main(void) {
+	double x = 48;
+	double y;
+
+	y = sqrt(x);
+	PR(1, "x = %g\n", x);
+	PR(2, "x = %.2f, y = %.4f\n", x, y);
+
+	return 0;
+}
